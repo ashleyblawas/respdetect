6dd9f1093fbc3a045aaecd88bd509a5cd340f7d8
Ashley Blawas <amb162@duke.edu>
2022-07-21 11:33:32 -0400

    Update get_contfR.m

--- respdetect/get_contfR.m
+++ respdetect/get_contfR.m
@@ -12,8 +12,21 @@ fR = 60./diff(breath_times); % Take diff of all breath times
 
 % Plot continuous fR with dive profile
 figure;
-stairs(breath_times(2:end)./60, fR, '.','MarkerSize', 6); 
+ax(1) = subplot(311);
+stairs(breath_times(2:end)./60, fR, 'ko','MarkerSize', 8, 'MarkerFaceColor', 'yellow'); 
+set(gca,'Xticklabel',[])
+ylabel('{\it f}_R (breaths min^{-1})');
+POS1 = get(ax(1), 'Position')
+POS1(2) = POS1(2)+0.05
+set(ax(1), 'Position', POS1) ;
 
 hold on; 
-plot(time_min, -p, 'k'); xlabel('Time (min)'); ylabel('Depth (m), {\it f}_R (breaths min^{-1})');
+ax(2) = subplot(3, 1, [2 3]);
+plot(time_min, p, 'k', 'LineWidth', 1.5); xlabel('Time (min)'); ylabel('Depth (m)');
+set(gca, 'Ydir', 'reverse');
+linkaxes(ax, 'x'); ylim([-1 max(p)]);
+POS = get(ax(2), 'Position')
+POS(4) = POS(4)+0.1
+set(ax(2), 'Position', POS) ;
+
 end
