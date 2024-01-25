c134cddf98c8915783b77f9aa99e8655f07c73ec
Ashley Blawas <amb162@duke.edu>
2024-01-24 17:30:27 -0800

    Dealing with NaNs...

--- respdetect/calc_move.m
+++ respdetect/calc_move.m
@@ -2,10 +2,17 @@
 
 function calc_move(fs, Aw, p, data_path, tag, pitch, roll, head)
  %Calculate filtered acceleration
-            %Rename Aw vector
+ 
+            % Rename Aw vector
             surge = Aw(:, 1);
             sway = Aw(:, 2);
             heave = Aw(:, 3);
+            
+            % Make temp variables to work with and then add back to full
+            % variables
+            surge_temp = surge(~isnan(surge));
+            sway_temp = sway(~isnan(sway));
+            heave_temp = heave(~isnan(heave));
             
             % Filter all three accel vectors, 5th order butterworth, with
             % passband between 2 Hz and 15 Hz
@@ -17,9 +24,10 @@ function calc_move(fs, Aw, p, data_path, tag, pitch, roll, head)
                 pass = [2, 15];
                 [b,a]=butter(5,pass/fny,'bandpass');
             end           
-            surge_filt = filtfilt(b,a, surge);
-            sway_filt = filtfilt(b,a, sway);
-            heave_filt = filtfilt(b,a, heave);
+            
+            surge_filt = filtfilt(b,a, surge_temp);
+            sway_filt = filtfilt(b,a, sway_temp);
+            heave_filt = filtfilt(b,a, heave_temp);
            
             % Get surge diff
             surge_diff = diff(surge_filt);
@@ -47,26 +55,63 @@ function calc_move(fs, Aw, p, data_path, tag, pitch, roll, head)
             
             % Build filter for prh
             fny = fs/2;
-
+            
+            % Make temp variables to work with and then add back to full
+            % variables
+            pitch_temp = pitch(~isnan(pitch));
+            roll_temp = roll(~isnan(roll));
+            head_temp = head(~isnan(head));
+            
             % Calculate filtered prh signals
-            pitch_filt = filtfilt(b, a, pitch);
-            roll_filt = filtfilt(b, a, roll);
-            head_filt = filtfilt(b, a, head);
+            pitch_filt = filtfilt(b, a, pitch_temp);
+            roll_filt = filtfilt(b, a, roll_temp);
+            head_filt = filtfilt(b, a, head_temp);
 
-            %Get prh diff
+            % Get prh diff
             pitch_diff = diff(pitch_filt);
             roll_diff = diff(roll_filt);
             head_diff = diff(head_filt);
 
-            %Get Shannon entropy
+            % Get Shannon entropy
             pitch_se = log(abs(pitch_diff))*sum(abs(pitch_diff));
             roll_se = log(abs(roll_diff))*sum(abs(roll_diff));
             head_se = log(abs(head_diff))*sum(abs(head_diff));
 
-            %Get smoothed Shannon entropy
+            % Get smoothed Shannon entropy
             pitch_smooth = movmean(pitch_se, 5*fs);
             head_smooth = movmean(head_se, 5*fs);
             roll_smooth = movmean(roll_se, 5*fs);
+            
+            % Replace temp variables back into full length variables
+            surge_smooth_temp = NaN(length(surge), 1);
+            sway_smooth_temp = NaN(length(sway), 1);
+            heave_smooth_temp = NaN(length(heave), 1);
+            jerk_smooth_temp = NaN(length(surge), 1);
+            jerk_filt_temp = NaN(length(surge), 1);
+            pitch_smooth_temp = NaN(length(pitch), 1);
+            head_smooth_temp = NaN(length(head), 1);
+            roll_smooth_temp = NaN(length(roll), 1);
+            
+            % But smoothed variables back in, append last value again to
+            % make same size
+            surge_smooth_temp(~isnan(surge)) = [surge_smooth; surge_smooth(end)];
+            sway_smooth_temp(~isnan(sway)) = [sway_smooth; sway_smooth(end)];
+            heave_smooth_temp(~isnan(heave)) = [heave_smooth; heave_smooth(end)];
+            jerk_smooth_temp(~isnan(surge)) = [jerk_smooth, jerk_smooth(end)];
+            jerk_filt_temp(~isnan(surge)) = [jerk_filt; jerk_filt(end)];
+            pitch_smooth_temp(~isnan(pitch)) = [pitch_smooth; pitch_smooth(end)];
+            head_smooth_temp(~isnan(head)) = [head_smooth; head_smooth(end)];
+            roll_smooth_temp(~isnan(roll)) = [roll_smooth; roll_smooth(end)];
+            
+            % Rename variables
+            surge_smooth = surge_smooth_temp;
+            sway_smooth = sway_smooth_temp;
+            heave_smooth = heave_smooth_temp;
+            jerk_smooth = jerk_smooth_temp;
+            jerk_filt = jerk_filt_temp;
+            pitch_smooth = pitch_smooth_temp;
+            head_smooth = head_smooth_temp;
+            roll_smooth = roll_smooth_temp;
 
             % Save all movement variables to mat file
             save(strcat(data_path, "\movement\", tag, "movement.mat"), 'p', 'Aw', 'surge', 'sway', 'heave',...
